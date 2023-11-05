1df2f77de15993eda034900295612fd3f1e7028f
shivam pandey <shivamvns111@gmail.com>
2023-11-05 22:35:38 +0530

    Added communication between toolbox and menu via redux toolkit

---
+++ src/components/Board/index.js
@@ -0,0 +1,16 @@
+import { useEffect, useRef } from "react";
+const Board = () => {
+    const canvasRef = useRef(null);
+    
+    useEffect(() => {
+        if (!canvasRef) return;
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+
+        //when mounting
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    },[])
+  return <canvas ref={canvasRef}></canvas>;
+};
+export default Board;
