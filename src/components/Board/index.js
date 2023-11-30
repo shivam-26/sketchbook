cdf90d68fbb3d3f310cfed6908c729a76e8fa839
shivam pandey <shivamvns111@gmail.com>
2023-11-30 07:22:26 +0530

    Added ability to download

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,69 +1,82 @@
 import { useEffect, useLayoutEffect, useRef } from "react";
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
+import { MENU_ITEMS } from "@/constants";
+import { menuItemClick, actionItemClick } from '@/slice/menuSlice'
+
 
 const Board = () => {
+  const dispatch = useDispatch()
   const canvasRef = useRef(null);
   const shouldDraw = useRef(false);
-  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+  const { activeMenuItem, actionMenuItem } = useSelector((state) => state.menu);
   const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
+   useEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    if (actionMenuItem === MENU_ITEMS.DOWNLOAD) {
+      const URL = canvas.toDataURL();
+      const anchor = document.createElement("a");
+      anchor.href = URL;
+      anchor.download = "sketch.png";
+      anchor.click();
+      console.log(URL);
+    }
+    dispatch(actionItemClick(null)) ;
+  }, [actionMenuItem,dispatch]);
 
   useEffect(() => {
     if (!canvasRef.current) return;
     const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
+    const context = canvas.getContext("2d");
 
     const changeConfig = () => {
-      context.strokeStyle = color
-      context.lineWidth = size
-    }
+      context.strokeStyle = color;
+      context.lineWidth = size;
+    };
     changeConfig();
-   
-  },[color,size])
-    
+  }, [color, size]);
+
   //before browser paint
-    useLayoutEffect(() => {
-        if (!canvasRef.current) return;
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
+  useLayoutEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
 
-        
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-      const beginPath = (x,y) => {
-        context.beginPath();
-        context.moveTo(x, y)
-      }
-      const drawLine = (x,y) => {
-        context.lineTo(x, y);
-        context.stroke();
-      }
-     const handleMouseDown = (e) => { 
-        shouldDraw.current = true;
-        beginPath(e.clientX,e.clientY);
-        
-      }
-      const handleMouseMove = (e) => { 
-        if (!shouldDraw.current) return;
-        drawLine(e.clientX,e.clientY)
-       
-      }
-     const handleMouseUp = (e) => {
-        shouldDraw.current = false;
-      }
-  
-      canvas.addEventListener('mousedown', handleMouseDown)
-      canvas.addEventListener('mousemove', handleMouseMove)
-      canvas.addEventListener('mouseup',handleMouseUp)
-      
-      
-      return () => {
-        canvas.addEventListener('mousedown', handleMouseDown)
-        canvas.addEventListener('mousemove', handleMouseMove)
-        canvas.addEventListener('mouseup',handleMouseUp)
-      }
-    }, [])
-  console.log(color,size)
+    const beginPath = (x, y) => {
+      context.beginPath();
+      context.moveTo(x, y);
+    };
+    const drawLine = (x, y) => {
+      context.lineTo(x, y);
+      context.stroke();
+    };
+    const handleMouseDown = (e) => {
+      shouldDraw.current = true;
+      beginPath(e.clientX, e.clientY);
+    };
+    const handleMouseMove = (e) => {
+      if (!shouldDraw.current) return;
+      drawLine(e.clientX, e.clientY);
+    };
+    const handleMouseUp = (e) => {
+      shouldDraw.current = false;
+    };
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      canvas.addEventListener("mousedown", handleMouseDown);
+      canvas.addEventListener("mousemove", handleMouseMove);
+      canvas.addEventListener("mouseup", handleMouseUp);
+    };
+  }, []);
+  console.log(color, size);
   return <canvas ref={canvasRef}></canvas>;
 };
 export default Board;
